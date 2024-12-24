2934389080bcfcebdc4471ddcb69f6c0e6f699b4
Sevinch <dekanovasevinchhh@gmail.com>
2024-12-24 10:36:09 +0500

    1ni tub son deb hisoblanmasligi uchun return qo'shildi

--- main.js
+++ main.js
@@ -1,160 +1,39 @@
-// 10. IIFE
-// (function() {
-//     console.log('My name is Sevinch');
-//   })();
-
-
-// // ozgaruvchi sifatida ishlatish
-// const ozgaruvchi = (function() {
-//     let localozgaruvchi = 'hello world';
-//     console.log(localozgaruvchi);
-//     return localozgaruvchi.toUpperCase();
-//   })();
-  
-// console.log(ozgaruvchi); 
-
-
-// // Arrow function sifatida
-// (() => {
-//     console.log('Arrow function');
-//   })();
-  
-
-
-
-
-// 11 MODE-STRICT
-// let this = 9;
-// console.log(this);
-
-// var function = 5;
-// console.log(function);
-
-
-
-
-
-//12  HIGHER ORDER FUNCTION
-// function calculate(operation, a, b) {
-//     return operation(a, b);
-//     // calculate nomli bu funksiya operationga berilgan buyrug'ini qaytaradi
-//   }
-  
-//   function add(x, y) {
-//     return x + y;
-//     // add nomli funksiya x ni y ga qo'shadi
-//   }
-  
-//   function multiply(x, y) {
-//     return x * y;
-//     // multiply nomli bu funksiya x ni y ga ko'paytiradi
-//   }
-  
-//   console.log(calculate(add, 5, 3)); 
-//   console.log(calculate(multiply, 5, 3)); 
-// //  bu yerda calculate funksiyani ishlatyapmiz lekin,operation argumentini o'rniga add va multiply funksiyalarni qo'yib ketyapmiz
-  
-
-// console.log("this is = " + this);
-
-// const developer = {
-//     firstname: "Sevinch",
-//     lastname: "Dekanova",
-//     id: 7, 
-//     hello: function() {
-//         return "My name is " + this.firstname;
-//     }
-// };
-
-// document.getElementsById("text").innerHTML = developer.hello();
-
-
-// function doSomething() {
-//     console.log(this);
-//     }
-//     doSomething();
-    
-
-
-
-// CALL()
-// const person = {
-//     fullName: function() {
-//       return `${this.firstName} ${this.lastName}`;
-//     }
-//   };
-  
-//   const person1 = { firstName: "John", lastName: "Doe" };
-//   const person2 = { firstName: "Jane", lastName: "Smith" };
-  
-//   console.log(person.fullName.call(person1)); // John Doe
-//   console.log(person.fullName.call(person2)); // Jane Smith
-  
-
-// Apply()
-
-// const numbers = [5, 6, 2, 8, 3];
-
-// const max = Math.max.apply(null, numbers);
-// const min = Math.min.apply(null, numbers);
-
-// console.log(max); // 8
-// console.log(min); // 2
-
-
-// Bind()
-
-// const person = {
-//     firstName: "John",
-//     lastName: "Doe",
-//     fullName: function() {
-//       return `${this.firstName} ${this.lastName}`;
-//     }
-//   };
-  
-//   const person1 = { firstName: "Jane", lastName: "Smith" };
-  
-//   const boundFunction = person.fullName.bind(person1);
-  
-//   console.log(boundFunction()); // Jane Smith
-
-
 
 
 // SONNI TUB YOKI TUB SON EMASLIGIGA TEKSHIRISH
 
 
+function tekshirish() { //tekshirish nomli funksiya ochamiz
 
-function tekshirish() {
+  const n = parseInt(document.getElementById("number").value);  //inputni qiymatini olish uchun id orqali o'zgaruvchiga tenglaymiz
+  const natijasi = document = document.getElementById("natija");  //natija chiquvchi tegni ham id orqali o'zgaruvchiga tenglab olamiz
 
-  const n = parseInt(document.getElementById("number").value);
-  const natijasi = document = document.getElementById("natija");
-
-  if (isNaN(n)) {
-    natijasi.textContent = "Iltimos, son kiriting!";
+  if (isNaN(n)) {  //inputning qiymatini son ekanligiga tekshiradi
+    natijasi.textContent = "Iltimos, son kiriting!";  // textContent  bu JavaScriptda DOM (Document Object Model) elementining ichidagi matnni olish yoki o'zgartirish uchun ishlatiladigan xususiyatdir.
     return;
 }
 
-if (n<=1){
-  natijasi.textContent = "Tub son emas";
+if (n <= 1){
+  natijasi.textContent = "Tub son emas"; // 1 va 1 dan kichik sonlar tub hisoblanmaydi
+  return;
 }
 
-let isPrime = true;
+let isPrime = true;   // isPrime avval true qiymat oladi ya'ni sonni tub deb qabul qiladi va keyin uni tekshiradi
 
-for (let i=2; i<= Math.sqrt(n); i++) {
-  if( n% i === 0) {
-    isPrime = false;
-    break;
+for (let i=2; i<= Math.sqrt(n); i++) {  //for sikli n ning kvadrat ildizigacha aylanadi,tub songa tekshirish qoidasiga ko'ra
+  if( n% i === 0) {   // qoldiqli bo'lishga tekshirish
+    isPrime = false;  // agar n sonini i songa bo'lganda qoldiq 0 bo'lsa demak uning bo'luvchisi bor degani bu esa tub emasligini bildiradi
+    break;   // agar tub son emasligi aniqlansa break siklni to'xtatadi 
 
   }
 
 }
 
 if(isPrime) {
-  natijasi.textContent = `${n} tub son`;
+  natijasi.textContent = `${n} tub son`;    //isaprime true bo'lsa tub son degan natija ekranda chiqadi
 }
 else{
-  natijasi.textContent = `${n} tub son emas`;
+  natijasi.textContent = `${n} tub son emas`;  // aksincha bo'lsa tub son emas yozuvi chiqadi
 }
 
 }
